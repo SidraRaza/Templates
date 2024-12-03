db2da9a005d4237328a4dacc35d4059ad6871a8e
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-12-03 12:47:39 +0500

    first commit

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import ResponsiveNav from "@/components/ResponsiveNav";
-
-
+import Customer from "@/components/Customer";
 export default function Home() {
   return (
   <main>
     <Header/>
     <ResponsiveNav/>
+  <Hero/>
+  <Customer/>
   </main>
   );
 }
---
+++ blog/src/components/Customer.tsx
@@ -0,0 +1,186 @@
+import Image from "next/image";
+import React from "react";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const Customer = () => {
+  return (
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 w-[90%] mx-auto pt-20">
+<div className="grid justify-center items-center">
+<Image
+            src={"/1.webp"}
+            alt="logo"
+            width={350}
+            height={200}
+            className="rounded-lg w-[350px] h-[250px]"
+          />  
+               <h1 className="text-gray-400 mt-6 text-sm"><b className="text-black">Business, Travel</b> — July 2, 2020</h1>
+            <h1 className="text-1xl font-bold w-[280px] mt-1 mb-2">
+              Your most unhappy customers are your greatest source of learning.
+            </h1>
+          <h1 className="text-gray-400 w-[290px] text-sm">  Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.</h1>
+            <div className="flex   items-center my-5">
+            <Image
+              src={"/person_1.jpg.webp"}
+              alt="logo"
+              width={50}
+              height={50}
+              className="rounded-full "
+            />
+            <div className="flex flex-col ml-3">
+              <h1 className="text-black font-bold">Sergy Campbell</h1>
+              <h1 className="text-gray-400 text-sm">CEO and Founder</h1>
+            </div>
+            </div>  
+</div>
+<div className="grid justify-center items-center">
+<Image
+            src={"/2.webp"}
+            alt="logo"
+            width={350}
+            height={200}
+            className="rounded-lg w-[350px] h-[250px]"
+          />  
+               <h1 className="text-gray-400 mt-6 text-sm"><b className="text-black">Business, Travel</b> — July 2, 2020</h1>
+            <h1 className="text-1xl font-bold w-[280px] mt-1 mb-2">
+              Your most unhappy customers are your greatest source of learning.
+            </h1>
+          <h1 className="text-gray-400 w-[290px] text-sm">  Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.</h1>
+            <div className="flex   items-center my-5">
+            <Image
+              src={"/person_1.jpg.webp"}
+              alt="logo"
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+            <div className="flex flex-col ml-3">
+              <h1 className="text-black font-bold">Sergy Campbell</h1>
+              <h1 className="text-gray-400 text-sm">CEO and Founder</h1>
+            </div>
+            </div>  
+</div>
+<div className="grid justify-center items-center">
+<Image
+            src={"/3.webp"}
+            alt="logo"
+            width={350}
+            height={200}
+            className="rounded-lg w-[350px] h-[250px]"
+          />  
+               <h1 className="text-gray-400 mt-6 text-sm"><b className="text-black">Business, Travel</b> — July 2, 2020</h1>
+            <h1 className="text-1xl font-bold w-[280px] mt-1 mb-2">
+              Your most unhappy customers are your greatest source of learning.
+            </h1>
+          <h1 className="text-gray-400 w-[290px] text-sm">  Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.</h1>
+            <div className="flex   items-center my-5">
+            <Image
+              src={"/person_1.jpg.webp"}
+              alt="logo"
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+            <div className="flex flex-col ml-3">
+              <h1 className="text-black font-bold">Sergy Campbell</h1>
+              <h1 className="text-gray-400 text-sm">CEO and Founder</h1>
+            </div>
+            </div>  
+</div>
+<div className="grid justify-center items-center">
+<Image
+            src={"/3.webp"}
+            alt="logo"
+            width={350}
+            height={200}
+            className="rounded-lg w-[350px] h-[250px]"
+          />  
+               <h1 className="text-gray-400 mt-6 text-sm"><b className="text-black">Business, Travel</b> — July 2, 2020</h1>
+            <h1 className="text-1xl font-bold w-[280px] mt-1 mb-2">
+              Your most unhappy customers are your greatest source of learning.
+            </h1>
+          <h1 className="text-gray-400 w-[290px] text-sm">  Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.</h1>
+            <div className="flex   items-center my-5">
+            <Image
+              src={"/person_1.jpg.webp"}
+              alt="logo"
+              width={50}
+              height={50}
+              className="rounded-full "
+            />
+            <div className="flex flex-col ml-3">
+              <h1 className="text-black font-bold">Sergy Campbell</h1>
+              <h1 className="text-gray-400 text-sm">CEO and Founder</h1>
+            </div>
+            </div>  
+</div>
+<div className="grid justify-center items-center">
+<Image
+            src={"/5.webp"}
+            alt="logo"
+            width={350}
+            height={200}
+            className="rounded-lg w-[350px] h-[250px]"
+          />  
+               <h1 className="text-gray-400 mt-6 text-sm"><b className="text-black">Business, Travel</b> — July 2, 2020</h1>
+            <h1 className="text-1xl font-bold w-[280px] mt-1 mb-2">
+              Your most unhappy customers are your greatest source of learning.
+            </h1>
+          <h1 className="text-gray-400 w-[290px] text-sm">  Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.</h1>
+            <div className="flex   items-center my-5">
+            <Image
+              src={"/person_1.jpg.webp"}
+              alt="logo"
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+            <div className="flex flex-col ml-3">
+              <h1 className="text-black font-bold">Sergy Campbell</h1>
+              <h1 className="text-gray-400 text-sm">CEO and Founder</h1>
+            </div>
+            </div>  
+</div>
+<div className="grid justify-center items-center">
+<Image
+            src={"/6.webp"}
+            alt="logo"
+            width={350}
+            height={200}
+            className="rounded-lg w-[350px] h-[250px]"
+          />  
+               <h1 className="text-gray-400 mt-6 text-sm"><b className="text-black">Business, Travel</b> — July 2, 2020</h1>
+            <h1 className="text-1xl font-bold w-[280px] mt-1 mb-2">
+              Your most unhappy customers are your greatest source of learning.
+            </h1>
+          <h1 className="text-gray-400 w-[290px] text-sm">  Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.</h1>
+            <div className="flex   items-center my-5">
+            <Image
+              src={"/person_1.jpg.webp"}
+              alt="logo"
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+            <div className="flex flex-col ml-3">
+              <h1 className="text-black font-bold">Sergy Campbell</h1>
+              <h1 className="text-gray-400 text-sm">CEO and Founder</h1>
+            </div>
+            </div>  
+</div>
+
+    </div>
+  )
+}
+
+export default Customer
